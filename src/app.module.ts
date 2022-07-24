86c01e5f2593a057f38efd23e6cad75f1dfbb21d
CarlosZoft <carlosk2gunn@hotmail.com>
2022-07-24 16:02:11 -0300

    ✨ feat: added orders CRUD

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,21 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
+import { join } from 'path';
+import { SequelizeModule } from '@nestjs/sequelize';
 import { AppService } from './app.service';
+import { OrdersModule } from './orders/orders.module';
+import { Order } from './orders/entities/order.entity';
 
 @Module({
-  imports: [],
+  imports: [
+    SequelizeModule.forRoot({
+      dialect: 'sqlite',
+      host: join(__dirname, 'db.sqlite'),
+      autoLoadModels: true,
+      models: [Order],
+    }),
+    OrdersModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/orders/orders.service.ts
@@ -0,0 +1,37 @@
+import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/sequelize';
+import { CreateOrderDto } from './dto/create-order.dto';
+import { UpdateOrderDto } from './dto/update-order.dto';
+import { Order } from './entities/order.entity';
+
+@Injectable()
+export class OrdersService {
+  constructor(@InjectModel(Order) private orderModel: typeof Order) {}
+
+  create(createOrderDto: CreateOrderDto) {
+    return this.orderModel.create({
+      amount: createOrderDto.amount,
+    });
+  }
+  findAll() {
+    return this.orderModel.findAll();
+  }
+
+  findOne(id: string) {
+    return this.orderModel.findByPk(id, { rejectOnEmpty: true });
+  }
+
+  async update(id: string, updateOrderDto: UpdateOrderDto) {
+    const order = await this.findOne(id);
+
+    return order.update(updateOrderDto, {
+      where: { id },
+    });
+  }
+
+  async remove(id: string) {
+    const order = await this.findOne(id);
+
+    return order.destroy();
+  }
+}
